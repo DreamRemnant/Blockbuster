aa33c2bb9f02c140b624bd3e19dde52725936d10
Juan Martinez <jfmg.64@gmail.com>
2023-11-25 14:06:25 -0700

    Se creó una base de datos con DBContext y se añadieron usuarios

---
+++ Blockbuster.API/Controllers/UsuariosController.cs
@@ -0,0 +1,22 @@
+﻿using Blockbuster.API.Models;
+using Blockbuster.API.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blockbuster.API.Controllers
+{
+    public class UsuariosController:MyController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsuariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Usuario>> Get()
+        {
+            return _context.Usuarios.ToList();
+        }
+    }
+}
---
+++ Blockbuster.API/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Blockbuster.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Blockbuster.API.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
