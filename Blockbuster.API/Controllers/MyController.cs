b30dfb3349e7ddee49a857ad34729492dc2c5c05
Juan Martinez <jfmg.64@gmail.com>
2023-11-25 12:25:47 -0700

    Agregar archivos de proyecto.

---
+++ Blockbuster.API/Controllers/MyController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blockbuster.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MyController:ControllerBase
+    {
+    }
+}
---
+++ Blockbuster.API/Controllers/PeliculasController.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blockbuster.API.Controllers
+{
+    public class PeliculasController : MyController
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hola desde API");
+        }
+
+        [HttpGet("peliculas2")]
+        public ActionResult<string> Get2()
+        {
+            return Ok("Hola desde API de nuevo");
+        }
+
+        [HttpGet("{nombre}")]
+        public ActionResult<string> Get(string nombre)
+        {
+            return Ok($"Hola desde API {nombre}");
+        }
+
+        [HttpGet("{nombre}/{edad:int}")]
+        public ActionResult<string> Get(string nombre, int edad)
+        {
+            return Ok($"Hola desde API {nombre} {edad}");
+        }
+    }
+}
